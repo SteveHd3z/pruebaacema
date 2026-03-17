50f6011016ce5468dfe2b1038e089e25da0f7deb
Steve <edxteve@gmail.com>
2026-03-17 16:19:46 -0500

    Add styles and pagination to the dashboard home

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../card/Card";
 import { useAppContext } from "../../../context/appContext";
+import { Pagination } from "@mui/material";
+import "./Home.css";
 
+const USERS_PER_PAGE = 10;
 
 function Home() {
     const { users } = useAppContext();
+    const [page, setPage] = useState(1);
 
-
-    users.map(user => console.log(user)); // Imprime cada usuario en la consola
-    console.log(users);
-
-
-
+    const totalPages = Math.ceil(users.length / USERS_PER_PAGE);
+    const start = (page - 1) * USERS_PER_PAGE;
+    const currentUsers = users.slice(start, start + USERS_PER_PAGE);
 
     return (
-        <div>
+        <div className="home-container">
             <h1>Meet the Users!</h1>
-            {users.map((u, index) => (
-                <Card
-                    key={index}
-                    photo={u.user.picture.large}
-                    name={u.user.name.first + " " + u.user.name.last}
-                    email={u.user.email}
-                    phone={u.user.phone}
-                    location={u.user.location.city + ", " + u.user.location.state}
+            <div className="cards-grid">
+                {currentUsers.map((u, index) => (
+                    <Card
+                        key={start + index}
+                        photo={u.user.picture.large}
+                        name={u.user.name.first + " " + u.user.name.last}
+                        email={u.user.email}
+                        phone={u.user.phone}
+                        location={u.user.location.city + ", " + u.user.location.state}
+                    />
+                ))}
+            </div>
+            <div className="pagination-wrapper">
+                <Pagination
+                    count={totalPages}
+                    page={page}
+                    onChange={(_, value) => { setPage(value); window.scrollTo(0, 0); }}
+                    color="primary"
+                    shape="rounded"
                 />
-            ))}
+            </div>
         </div>
     );
 }
